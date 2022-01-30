e83b7855f95614474287b0834ccf9e45a78d1886
Juan Moreno <morenostudent1@gmail.com>
2022-01-30 13:02:06 -0800

    CHANGE: footer layout

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 const menuBtn = document.querySelector('.nav-toggle').addEventListener('click', toggleNav);
 const menu = document.querySelector('.nav-list');
 const footerCopyright = document.getElementById('copyright')
-const footerCopyrightMobile = document.getElementById('copyrightMobile')
 
 function toggleNav(){
     menu.classList.toggle('show')
@@ -12,6 +11,4 @@ const footerYear = new Date().getFullYear()
 footerCopyright.innerHTML += `<p class="text-primary">Copyright &copy; ${footerYear} | Website Design by <a
 href="https://www.juanmoreno.dev/">Juan Moreno</a></p>`
 
-footerCopyrightMobile.innerHTML += `<p class="text-primary">Copyright &copy; ${footerYear} | Website Design by <a
-href="https://www.juanmoreno.dev/">Juan Moreno</a></p>`
 
