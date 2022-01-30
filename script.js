dbbc6a5299856120ac4e85dc7e8025b0639194e4
Juan Moreno <morenostudent1@gmail.com>
2022-01-30 11:42:24 -0800

    Change: footer js copyright year

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const menuBtn = document.querySelector('.nav-toggle').addEventListener('click', toggleNav);
 const menu = document.querySelector('.nav-list');
 const footerCopyright = document.getElementById('copyright')
+const footerCopyrightMobile = document.getElementById('copyrightMobile')
 
 function toggleNav(){
     menu.classList.toggle('show')
@@ -9,5 +10,8 @@ function toggleNav(){
 
 const footerYear = new Date().getFullYear()
 footerCopyright.innerHTML += `<p class="text-primary">Copyright &copy; ${footerYear} | Website Design by <a
+href="https://www.juanmoreno.dev/">Juan Moreno</a></p>`
+
+footerCopyrightMobile.innerHTML += `<p class="text-primary">Copyright &copy; ${footerYear} | Website Design by <a
 href="https://www.juanmoreno.dev/">Juan Moreno</a></p>`
 
