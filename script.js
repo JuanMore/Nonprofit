2c55c9944b9d2b1c6d0f78205798e525d01fea1a
Juan Moreno <morenostudent1@gmail.com>
2022-01-31 02:11:36 -0800

    WRITE: js to close mobile nav on outisde click

--- script.js
+++ script.js
@@ -8,22 +8,11 @@ function toggleNav(){
 }
 
 // define a function to close menu on outside window click
-window.addEventListener( 'click', function( e ) {
-   const targetElement = e.target;  // clicked element
-
-    do {
-        if ( targetElement === menu ) {
-            // This is a click inside. Do nothing, just return.
-            return;
-        }
-
-        // Go up the DOM
-        targetElement = targetElement.parentNode;
-    } while ( targetElement );
-
-    // This is a click outside.
-    menu.click();
-} );
+document.onclick = function(e){
+    if(e.target.id !== menuBtn){
+        menu.classList.remove('show')
+    }
+}
 
 
 
